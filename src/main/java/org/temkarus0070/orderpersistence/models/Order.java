d209a7dea49138e0e8023b67b8a8395873bd522f
temkarus0070 <pretkovartyom@gmail.com>
2021-12-18 13:08:34 +0300

    remove fields, those not exists at kafka message

--- src/main/java/org/temkarus0070/orderpersistence/models/Order.java
+++ src/main/java/org/temkarus0070/orderpersistence/models/Order.java
@@ -33,26 +33,10 @@ public class Order{
         this.orderNum = orderNum;
     }
 
-    public Collection<Good> getGoods() {
-        return goods;
-    }
 
-    public void setGoods(Collection<Good> goods) {
-        this.goods = goods;
-    }
 
-    public Status getStatus() {
-        return status;
-    }
 
-    public void setStatus(Status status) {
-        this.status = status;
-    }
 
-    @OneToMany(mappedBy = "order")
-    private Collection<org.temkarus0070.orderpersistence.models.Good> goods;
-
-    private org.temkarus0070.orderpersistence.models.Status status;
 
 
 }
